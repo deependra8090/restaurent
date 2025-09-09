277323f199496aea00132f0c8d3152b289b62a43
deependra8090 <deependra9444@gmail.com>
2025-09-09 23:51:45 +0530

    script.js uploaded

---
+++ script.js
@@ -0,0 +1,23 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slideshow img');
+const dots = document.querySelectorAll('.dots span');
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove('active');
+    dots[i].classList.remove('active');
+    if (i === index) {
+      slide.classList.add('active');
+      dots[i].classList.add('active');
+    }
+  });
+  currentSlide = index;
+}
+
+setInterval(() => {
+  showSlide((currentSlide + 1) % slides.length);
+}, 4000);
+
+dots.forEach((dot, i) =>
+  dot.addEventListener('click', () => showSlide(i))
+);
