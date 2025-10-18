2e1770c95b0491921a20a00b6d55dd925d57b57a
deependra8090 <deependra9444@gmail.com>
2025-10-18 22:58:25 +0530

    Implement mobile menu toggle feature
    Added mobile menu toggle functionality.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Slideshow logic
 let currentSlide = 0;
 const slides = document.querySelectorAll('.slideshow img');
 const dots = document.querySelectorAll('.dots span');
@@ -18,6 +19,14 @@ setInterval(() => {
   showSlide((currentSlide + 1) % slides.length);
 }, 4000);
 
-dots.forEach((dot, i) =>
-  dot.addEventListener('click', () => showSlide(i))
-);
+dots.forEach((dot, i) => {
+  dot.addEventListener('click', () => showSlide(i));
+});
+
+// Mobile menu toggle
+const menuToggle = document.getElementById('menu-toggle');
+const navbar = document.getElementById('navbar');
+
+menuToggle.addEventListener('click', () => {
+  navbar.classList.toggle('active');
+});
